350644d78dcc5b0a1b2477f5520cc86258f5fb0d
DevOps-engineer-dev <beinghamza806@gmail.com>
2026-06-23 18:41:42 +0500

    Changed the event trigger type

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ mongoose
 
 // 3. NEW: Add a console.log to test load balancing
 app.get("/api/v1", (req, res) => {
-  res.send("<h2>Hello from the Node API! May GitHub Actions runs successfullys</h2>");
+  res.send("<h2>Hello from the Node API!</h2>");
   console.log("Yeah it ran"); // This helps prove Nginx is balancing traffic
 });
 
